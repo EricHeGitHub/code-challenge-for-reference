fa93a491fa98373c3c12bacfce06da7ba182d02d
Jiawei He <nmghejiawei@hotmail.com>
2019-05-19 21:49:49 +1000

    update python MRO

--- Others/PythonMRO.py
+++ Others/PythonMRO.py
@@ -13,6 +13,11 @@ class Triangle:
     def area(self):
         print("area in Triangle is called. The area is {0}".format(0.5 * self.height * self.base))
 
+    def __repr__(self):
+        return "repr is called"
+
+    def __str__(self):
+        return "str is called"
 
 class MyShape(Square, Triangle):
     def __init__(self, edge, height, base):
@@ -30,3 +35,4 @@ class MyShape(Square, Triangle):
 
 ms = MyShape(1,2,2)
 ms.area()
+print(ms)
