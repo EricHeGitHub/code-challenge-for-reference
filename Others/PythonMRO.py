832a15f58fa719add1b617e0c99608ccbce0ea22
Jiawei He <nmghejiawei@hotmail.com>
2019-05-19 17:34:30 +1000

    add python MRO test cases

---
+++ Others/PythonMRO.py
@@ -0,0 +1,32 @@
+class Square:
+    def __init__(self, edge):
+        self.edge = edge
+    def area(self):
+        print("area in Square is called. The area is {0}".format(self.edge ** 2))
+
+
+class Triangle:
+    def __init__(self, height, base):
+        self.height = height
+        self.base = base
+
+    def area(self):
+        print("area in Triangle is called. The area is {0}".format(0.5 * self.height * self.base))
+
+
+class MyShape(Square, Triangle):
+    def __init__(self, edge, height, base):
+        Square.__init__(self,edge)
+        Triangle.__init__(self, height, base)
+
+    def area(self):
+        Square.area(self) #must pass self for parent class
+        Triangle.area(self)
+        MyShape.staticMethod() #not need to pass self to parent class
+
+    def staticMethod():
+        print("Static Method is called")
+
+
+ms = MyShape(1,2,2)
+ms.area()
